2458d06e0733e12b0a65f0fb567061524e6d45fa
Dzmitry Plashchynski <plashchynski@gmail.com>
2022-07-04 20:53:07 +0300

    Add new device page

--- routes/devices.js
+++ routes/devices.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('devices');
+  res.render('devices/index');
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('devices/new');
 });
 
 module.exports = router;
