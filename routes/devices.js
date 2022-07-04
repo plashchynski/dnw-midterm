eb8d826e90540bc58a4882c7970d09c212b0c730
Dzmitry Plashchynski <plashchynski@gmail.com>
2022-07-04 22:03:38 +0300

    Add a favicon and all pages

--- routes/devices.js
+++ routes/devices.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('devices/index');
+router.get('/status', (req, res, next) => {
+  res.render('devices/status');
 });
 
 router.get('/new', (req, res, next) => {
   res.render('devices/new');
+});
+
+router.get('/control', (req, res, next) => {
+  res.render('devices/control');
+});
+
+router.get('/delete', (req, res, next) => {
+  res.render('devices/delete');
 });
 
 module.exports = router;
