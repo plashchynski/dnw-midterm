a114620503449d1f93ae81fa32b7ccbca02f5467
Dzmitry Plashchynski <plashchynski@gmail.com>
2022-07-11 14:26:15 +0300

    Add comments

--- services/db.js
+++ services/db.js
@@ -1,8 +1,11 @@
+// this file implements the database client
+
 const mysql = require('mysql2/promise');
 const config = require('../config');
 
 const pool = mysql.createPool(config.db);
 
+// perform a database query and return rows
 async function query(sql, params) {
   const [rows, fields] = await pool.execute(sql, params);
 
--- services/notifications.js
+++ services/notifications.js
@@ -1,11 +1,16 @@
+// This file implements notifications from the server-side to the client-side
+
+// list of subscribed clients
 if (!global.listeners) {
   global.listeners = [];
 }
 
+// send a message to all subscribed clients
 function broadcast(data) {
   global.listeners.forEach((listener) => listener.response.write(`data: ${JSON.stringify(data)}\n\n`));
 }
 
+// add a new subscribed client (a response object)
 function addListener(response) {
   const listenerId = Date.now();
 
@@ -19,6 +24,7 @@ function addListener(response) {
   return listenerId;
 }
 
+// remove a client from the subscription list
 function removeListener(listenerId) {
   global.listeners.filter((listener) => listener.id !== listenerId);
 }
